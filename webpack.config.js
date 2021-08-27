76e0ab7b88d2df79a38b668d9ec3b25a169cf05a
jbkang <kjb9301@naver.com>
2021-08-27 19:21:41 +0900

    html-webpack-plugin test

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   mode: 'production',
@@ -22,6 +23,11 @@ const config = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output management',
+    }),
+  ],
 };
 
 module.exports = config;
