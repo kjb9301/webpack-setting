164dfcac44d9513aefc2027092af996c900c697f
jbkang <kjb9301@naver.com>
2021-08-27 18:59:16 +0900

    iwebpack load image & test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import './style.css';
+import image from './testImage.png';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = 'Test Webpack';
   element.classList.add('hello');
+
+  const myImage = new Image();
+  myImage.src = image;
+
+  element.appendChild(myImage);
 
   return element;
 }
