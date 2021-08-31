fbd190eac02fe33aaa5ed84ca4b28e7bfdd0cbe3
jbkang <kjb9301@naver.com>
2021-08-31 20:48:32 +0900

    testing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import './style.css';
-import image from './testImage.png';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'Test Webpack';
-  element.classList.add('hello');
-
-  const myImage = new Image();
-  myImage.src = image;
-
-  element.appendChild(myImage);
-
-  return element;
-}
-
-document.getElementById('app').appendChild(component());
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const PROJECT = path.resolve(__dirname);
+console.log(PROJECT);
+
 const config = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   entry: {
-    app: './src/index.js',
+    app: './src/index',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/dist',
     clean: true,
   },
   module: {
     rules: [
+      {
+        test: /\.js(x?)/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
